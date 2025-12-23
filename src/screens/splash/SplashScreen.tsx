a170b77191c3404e4fca9bdfcf2919532a7c344c
Haseeb <haseeb@Haseebs-MacBook-Pro.local>
2025-12-24 03:06:51 +0500

    splashscreen

---
+++ src/screens/splash/SplashScreen.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, View, ImageBackground, StatusBar } from 'react-native';
+
+const SplashScreen = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+      <ImageBackground
+        source={require('@/assets/img/SplashScreen.png')} // Aapki Splash Screen V1.jpg image
+        style={styles.background}
+        resizeMode="cover"
+      />
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
